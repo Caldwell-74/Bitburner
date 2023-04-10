696aff8009079793746c4d5edec167f949d6ec06
zerbosh <115591472+zerbosh@users.noreply.github.com>
2023-04-10 17:56:48 +0200

    Create ButtonConstants.js

---
+++ Button/ButtonConstants.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  background: ['#141E2C', '#000000'], //[OFF,ON]
+  border: '#8b74f2',
+  button: '#8b74f2',
+  text: '#8b74f2',
+};
+export const CONSTANTS = {
+  defaultServer: 'home',
+  defaultThreads: 1,
+  restart: 'light', //"hard" = delete old buttons and create a full new set, "light" = only create new buttons
+
+  saveFile: 'buttonConfigs.txt',
+
+  font: 'Lucida Console',
+};
